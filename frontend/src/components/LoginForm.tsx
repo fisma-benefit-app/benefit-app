1e42ca4b3d90cf1f8f83297fe5dfd9b140ec3bf5
Textiili <erkki.tiili@gmail.com>
2025-02-14 12:13:31 +0200

    feat: loginError-message

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,31 +1,51 @@
 import { useState, FormEvent } from 'react';
 import useAppUser from '../hooks/useAppUser';
 import { fetchJWT } from '../api/authorization';
-import { fetchAllProjects } from '../api/projects';
 
 export default function LoginForm() {
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [rememberMe, setRememberMe] = useState<boolean>(false);
     const [showPassword, setShowPassword] = useState<boolean>(false);
-    const { appUser, loggedIn, sessionToken, setSessionToken } = useAppUser();
+    const [loginError, setLoginError] = useState<string | null>(null)
+    const [showLoginError, setShowLoginError] = useState<boolean>(false);
+    const { sessionToken, setSessionToken } = useAppUser();
 
     const login = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setLoginError(null);
 
         const loginToken = await fetchJWT(username, password);
 
         if (!loginToken) {
-            //TODO: Login failed prompt!
+            setLoginError('Kirjautuminen epäonnistui! Tarkista käyttäjänimi ja salasana.');
+            setShowLoginError(true);
+
+            setTimeout(() => {
+                setShowLoginError(false);
+                setTimeout(() => setLoginError(null), 500); 
+            }, 2500);
+            return;
         }
 
         setSessionToken(loginToken);
+        if (sessionToken)
+            alert("Kirjautuminen onnistui!");
+            //TODO: Redirect to session!
     };
 
     return (
         <>
             <form onSubmit={login} className="max-w-sm mx-auto p-4 border-2 border-gray-400 shadow-md ">
                 <h1 className="text-2xl text-gray-700 font-bold mb-4">Kirjaudu sisään</h1>
+
+                <div className="h-8 mb-4 flex items-center justify-center">
+                    {loginError && (
+                        <label className={`text-sm text-red-700 bg-red-100 border border-red-400 p-1 rounded transition-opacity duration-500 ease-in-out ${showLoginError ? 'opacity-100' : 'opacity-0'}`}>
+                            {loginError}
+                        </label>
+                     )}
+                </div>
 
                 <div className="mb-4 relative">
                     {/*                 <UserIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-500" /> */}
@@ -77,17 +97,7 @@ export default function LoginForm() {
                 >
                     Kirjaudu
                 </button>
-
             </form>
-
-            {/* TODO REMOVE TEST BUTTON! */}
-            <button
-                type="submit"
-                onClick={() => fetchAllProjects(sessionToken)}
-                className="w-full bg-sky-600 text-white p-2 hover:bg-zinc-600"
-            >
-                ANNA PROJEKTIT
-            </button>
         </>
     );
 }
