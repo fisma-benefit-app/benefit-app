841a47fb2563f29de5c36083537cc715f8410698
bhd433 <oskari.aho@myy.haaga-helia.fi>
2025-04-23 12:09:23 +0300

    bug fixes

--- frontend/src/components/ProjectPage.tsx
+++ frontend/src/components/ProjectPage.tsx
@@ -177,7 +177,7 @@ export default function ProjectPage() {
               {translation.saveProjectAsVersion} {project?.version}
             </button>
             <select
-              className="border-2 border-gray-400 px-4 py-4 cursor-pointer my-2 sticky top-60"
+              className="border-2 border-gray-400 px-4 py-4 cursor-pointer my-2"
               onChange={handleVersionSelect}
               defaultValue=""
             >
