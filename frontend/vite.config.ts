1ce3c8d650dbd5f0e16d32de023d33405d00ed49
devaajanne <devaajanne@gmail.com>
2025-09-09 12:06:57 +0300

    issue #214 * check if VITE_API_URL is set in vite.config.ts * apply Prettier formatting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/benefit-app/",
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+export default defineConfig(({ mode }) => {
+  // Load env variables with prefix "VITE_" by default for the current mode (e.g., "development")
+  const env = loadEnv(mode, process.cwd());
+
+  // Check if VITE_API_URL is set
+  if (!env.VITE_API_URL) {
+    throw new Error("VITE_API_URL is not set in your .env files!");
+  }
+
+  return {
+    base: "/benefit-app/",
+    plugins: [react(), tailwindcss()],
+  };
+});
