7e14ec5ec564955fb8c9f510b54c2e79652d6cef
devaajanne <devaajanne@gmail.com>
2025-09-13 19:04:46 +0300

    issue/#240-lisaa-BE-formatointi-checki-CI/CDeen * add Spotless Gradle plugin to build.gradle * apply Google Java Format formatting to backend code * add Spotless formatting check to GitHub actions workflow * clarify which formatting check targets which language

--- backend/src/main/java/fi/fisma/backend/repository/AppUserRepository.java
+++ backend/src/main/java/fi/fisma/backend/repository/AppUserRepository.java
@@ -1,9 +1,8 @@
 package fi.fisma.backend.repository;
 
+import fi.fisma.backend.domain.AppUser;
 import org.springframework.data.repository.ListCrudRepository;
 
-import fi.fisma.backend.domain.AppUser;
-
 public interface AppUserRepository extends ListCrudRepository<AppUser, Long> {
-    AppUser findByUsername(String username);
+  AppUser findByUsername(String username);
 }
--- backend/src/main/java/fi/fisma/backend/security/UserDetailsServiceImpl.java
+++ backend/src/main/java/fi/fisma/backend/security/UserDetailsServiceImpl.java
@@ -2,6 +2,9 @@ package fi.fisma.backend.security;
 
 import fi.fisma.backend.domain.AppUser;
 import fi.fisma.backend.repository.AppUserRepository;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -10,38 +13,32 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
-    private final AppUserRepository appUserRepository;
-    
+  private final AppUserRepository appUserRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    AppUser appUser = this.appUserRepository.findByUsername(username);
+    if (appUser == null) {
+      throw new UsernameNotFoundException("username " + username + " is not found");
+    }
+    return new AppUserDetails(appUser);
+  }
+
+  static final class AppUserDetails extends AppUser implements UserDetails {
+
+    private static final List<GrantedAuthority> ROLE_USER =
+        Collections.unmodifiableList(AuthorityUtils.createAuthorityList("ROLE_USER"));
+
+    AppUserDetails(AppUser appUser) {
+      super(appUser.getId(), appUser.getUsername(), appUser.getPassword());
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        AppUser appUser = this.appUserRepository.findByUsername(username);
-        if (appUser == null) {
-            throw new UsernameNotFoundException("username " + username + " is not found");
-        }
-        return new AppUserDetails(appUser);
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+      return ROLE_USER;
     }
-    
-    static final class AppUserDetails extends AppUser implements UserDetails {
-        
-        private static final List<GrantedAuthority> ROLE_USER = Collections
-                .unmodifiableList(AuthorityUtils.createAuthorityList("ROLE_USER"));
-        
-        AppUserDetails(AppUser appUser) {
-            super(appUser.getId(), appUser.getUsername(), appUser.getPassword());
-        }
-        
-        @Override
-        public Collection<? extends GrantedAuthority> getAuthorities() {
-            return ROLE_USER;
-        }
-        
-    }
-    
+  }
 }
