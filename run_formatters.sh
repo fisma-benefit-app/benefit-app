84ea911d3e489f0b64e384ad007ec510dbc9ecfc
devaajanne <devaajanne@gmail.com>
2025-09-16 08:34:23 +0300

    issue/#247-lisaa-pre-push-hook * add a pre-push script * add a script to run both FE and BE formatters

---
+++ run_formatters.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check if frontend is formatted according to Prettier, and format if not
+cd frontend
+if ! npx prettier . --check; then
+    echo "Frontend files are not formatted. Running Prettier formatter..."
+    npx prettier . --write
+    echo "Frontend files formatted! Please review changes."
+# else
+#     no need for an extra message here, Prettier generates one automatically
+fi
+cd ..
+
+# Check if backend is formatted according to Google Java Format, and format if not
+cd backend
+if ! ./gradlew spotlessCheck; then
+    echo "Backend files are not formatted. Running Spotless formatter..."
+    ./gradlew spotlessApply
+    echo "Backend files formatted! Please review changes."
+else
+    echo "All backend files use Google Java Format style!"
+fi
+cd ..
+
