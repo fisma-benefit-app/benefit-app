ac075221c96ae66fd12cfce9cefeaacafeac6962
devaajanne <devaajanne@gmail.com>
2025-09-30 16:03:38 +0300

    issue/#229-spring-bootin-pitaisi-antaa-viesti-kun-backend-on-buildattu * add console prints when Spring Boot has started

--- backend/src/main/java/fi/fisma/backend/BackendApplication.java
+++ backend/src/main/java/fi/fisma/backend/BackendApplication.java
@@ -8,5 +8,8 @@ public class BackendApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(BackendApplication.class, args);
+    System.out.println("\n===============================");
+    System.out.println("SPRING BOOT APPLICATION STARTED");
+    System.out.println("===============================\n");
   }
 }
